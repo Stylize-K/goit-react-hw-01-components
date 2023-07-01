f985c8681b82ab9da1b05ba659f1d73631441781
Kyrylo Lysenko <lysenko.ko@gmail.com>
2023-07-02 01:05:10 +0300

    Change main conponent App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,14 @@
+import user from "../data/user.json";
+import data from "../data/data.json";
+import friends from "../data/friends.json";
+import transactions from "../data/transactions.json"
+
+import { Profile } from './Profile/Profile'
+import { Statistics } from "./Statistics/Statistics";
+import { FriendList } from "./FriendList/FriendList";
+import { TransactionHistory } from "./TransactionHistory/TransactionHistory";
+
+
 export const App = () => {
   return (
     <div
@@ -9,8 +20,18 @@ export const App = () => {
         fontSize: 40,
         color: '#010101'
       }}
-    >
-      React homework template
+    > <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        followers={user.stats.followers}
+        views={user.stats.views}
+        likes={user.stats.likes}
+      />
+      <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </div>
   );
 };
