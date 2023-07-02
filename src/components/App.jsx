40e0b52a4cc5fd4f6a4f0fe69d549871d6577aa4
Kyrylo Lysenko <lysenko.ko@gmail.com>
2023-07-02 19:39:08 +0300

    Change some App inline styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,26 +1,28 @@
-import user from "../data/user.json";
-import data from "../data/data.json";
-import friends from "../data/friends.json";
-import transactions from "../data/transactions.json"
+import user from '../data/user.json';
+import data from '../data/data.json';
+import friends from '../data/friends.json';
+import transactions from '../data/transactions.json';
 
-import { Profile } from './Profile/Profile'
-import { Statistics } from "./Statistics/Statistics";
-import { FriendList } from "./FriendList/FriendList";
-import { TransactionHistory } from "./TransactionHistory/TransactionHistory";
+import { Profile } from './Profile/Profile';
+import { Statistics } from './Statistics/Statistics';
+import { FriendList } from './FriendList/FriendList';
+import { TransactionHistory } from './TransactionHistory/TransactionHistory';
 
+const rootStyles = {
+  // height: '100vh',
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '35px',
+  justifyContent: 'center',
+  alignItems: 'center',
+  fontSize: 40,
+  color: '#010101',
+};
 
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    > <Profile
+    <div style={rootStyles}>
+      <Profile
         username={user.username}
         tag={user.tag}
         location={user.location}
