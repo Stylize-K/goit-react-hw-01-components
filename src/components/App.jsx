19d0948d430baff649ad6096a2172da3a27951dd
Kyrylo Lysenko <lysenko.ko@gmail.com>
2023-07-03 01:05:22 +0300

    Change inline styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -12,7 +12,7 @@ const rootStyles = {
   // height: '100vh',
   display: 'flex',
   flexDirection: 'column',
-  gap: '35px',
+  gap: '40px',
   justifyContent: 'center',
   alignItems: 'center',
   fontSize: 40,
