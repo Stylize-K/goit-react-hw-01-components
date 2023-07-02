6d026e77f34d1b57888990910af711e98a4dbe7f
Kyrylo Lysenko <lysenko.ko@gmail.com>
2023-07-03 01:06:50 +0300

    Add getRandomHexColor function

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -7,7 +7,11 @@ export const Statistics = ({ title, stats }) => {
       {title && <h2 className={css.title}>{title}</h2>}
       <ul className={css.statList}>
         {stats.map(item => (
-          <li key={item.id} className={css.item}>
+          <li
+            key={item.id}
+            className={css.item}
+            style={{ backgroundColor: getRandomHexColor() }}
+          >
             <span className={css.label}>{item.label}</span>
             <span className={css.percentage}>{item.percentage}%</span>
           </li>
@@ -26,4 +30,10 @@ Statistics.propTypes = {
       percentage: PropTypes.number.isRequired,
     })
   ).isRequired,
+};
+
+const getRandomHexColor = () => {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 };
